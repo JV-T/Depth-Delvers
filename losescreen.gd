cd54c5e5d1131d14405124208a78cdd2bfb3cd8d
FRCJavisT <jtsai2@ocdsb.ca>
2026-03-01 08:29:29 -0500

    Update losescreen.gd

--- losescreen.gd
+++ losescreen.gd
@@ -5,7 +5,7 @@ func _process(delta: float) -> void:
 	if UserInterface.oxygen <= 0:
 		visible = true
 		get_tree().paused = true
-		$highscore.text = "Highscore: " + str(((UserInterface.downmeters + 1990) * -0.01) + -44 * UserInterface.level) + "m"
+		$highscore.text = "Highscore: " + str(UserInterface.downmeters) + "m"
 
 
 func _on_button_pressed() -> void:
