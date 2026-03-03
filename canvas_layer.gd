4e2fc3b95d5ad90149395fc99311e1079e561fcd
Geofoods <curious.george71511@gmail.com>
2026-03-03 18:23:53 -0500

    pause button and menu

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -9,3 +9,15 @@ func transition(scenepath):
 	get_tree().paused = false
 	if scenepath == "res://scenes/level.tscn":
 		LayerPopup.show_layer(1)
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().paused = !get_tree().paused
+	$Control.visible = !$Control.visible
+
+
+
+
+func _on_button_pressed() -> void:
+	get_tree().paused = false
+	$Control.visible = false
